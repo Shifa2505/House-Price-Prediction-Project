cd9509191abb75bb0b5b61c06a18cf5c51278074
Shifa25 <shifashaikh2505@gmail.com>
2023-03-13 23:39:57 +0530

    gui completed

---
+++ house_price/main.py
@@ -0,0 +1,35 @@
+# flask, scikit-learn, pandas, pickle-mixin
+import pandas as pd
+from flask import Flask, render_template, request
+import pickle
+app = Flask(__name__)
+data = pd.read_csv('Cleaned_data.csv')
+pipe = pickle.load(open("RidgeModel.pkl", 'rb'))
+
+from sklearn.preprocessing import OneHotEncoder
+
+encoder = OneHotEncoder(handle_unknown='ignore')
+
+@app.route('/')
+def index():
+
+    locations = sorted(data['location'].unique())
+    return render_template('index.html', locations=locations)
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    location = request.form.get('location')
+    bhk = request.form.get('bhk')
+    bathrooms = request.form.get('bathrooms')
+    sqft = request.form.get('total_sqft')
+
+    print(location,bhk,bathrooms,sqft)
+    input_data = pd.DataFrame([[location,sqft,bathrooms,bhk]], columns=['location', 'total_sqft', 'bathrooms', 'bhk'])
+    prediction = pipe.predict(input_data)[0]
+
+    return str(prediction)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
+
